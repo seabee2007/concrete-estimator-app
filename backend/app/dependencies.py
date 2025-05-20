edb8397b85ff39e7fd92b059482e648abf3997e4
seabee2007 <113162580+seabee2007@users.noreply.github.com>
2025-05-21 06:06:17 +1000

    Update dependencies.py

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,1 +1,11 @@
+from pydantic import BaseSettings
 
+
+class Settings(BaseSettings):
+    openweather_api_key: str
+    class Config:
+        env_file = ".env"
+
+
+def get_settings():
+    return Settings()
